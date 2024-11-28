57216643555059902fbf13a0e80e5aee79576dd5
Lazox12 <tomik.lazar@gmail.com>
2024-11-28 23:32:42 +0100

    fix

--- hardware/scripts/dump.sh
+++ hardware/scripts/dump.sh
@@ -1,1 +1,4 @@
+service hackathon stop
+rm ../db.sqlite3
 mysql2sqlite -f ../db.sqlite3 -d hackathon -t routes stops route_stops -u root --mysql-password hackathon -h 34.118.68.104
+service hackathon start
