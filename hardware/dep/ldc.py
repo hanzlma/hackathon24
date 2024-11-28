3d46f61fb7c418526145ff14053d450a0da3b734
Lazox12 <tomik.lazar@gmail.com>
2024-11-28 13:50:49 +0100

    fix

---
+++ hardware/dep/ldc.py
@@ -0,0 +1,22 @@
+from rpi_lcd import LCD as ldcController
+from time import sleep
+
+class lcd:
+    def __init__(self):
+        self.lcd = ldcController()
+
+    def __del__(self):
+        self.lcd.clear()
+
+    def display(self, text,line,pos='left'):
+        self.lcd.text(text,line,pos)
+
+    def setNext(self,stationName,stop):
+        self.display(" prsti zastavka:",2,'center')
+        self.display(stationName,3,'center')
+
+        self.display(("zastavime" if stop else "nezastavime"),4,'center')
+if __name__ == "__main__":
+    display = lcd()
+    display.setNext("brno",False)
+    sleep(10)
